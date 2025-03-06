a32702deeb414d71ca1210142a420ef98e13899e
Olman-dep <olmanzapata22@gmail.com>
2025-03-05 19:33:53 -0500

    Delete ClienteRepositorio.java

--- ClienteRepositorio.java
+++
@@ -1,7 +0,0 @@
-package Oz.zona_fit.Repositorio;
-
-import Oz.zona_fit.Modelo.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
-}
